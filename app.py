c994a8e66fc17d27a53c61e1b9763bb6799b061b
Abaid ur Rehman <mbilalzulfiqar876@gmail.com>
2026-03-10 12:34:08 +0500

    fix railway crash

--- app.py
+++ app.py
@@ -1315,4 +1315,5 @@ def update_password():
 #   Don't change this 
 #----------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
